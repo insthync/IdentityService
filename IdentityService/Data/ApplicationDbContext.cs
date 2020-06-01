bcb3652418935d4449c92d5974452dbb7d99f5bc
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2020-06-02 06:59:14 +0700

    First commit, just simple Identity with MySQL database

---
+++ IdentityService/Data/ApplicationDbContext.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityService.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<IdentityUser>().ToTable("users");
+            builder.Entity<IdentityUser>(entity => {
+                entity.Property(m => m.Id).HasMaxLength(127);
+                entity.Property(m => m.Email).HasMaxLength(127);
+                entity.Property(m => m.NormalizedEmail).HasMaxLength(127);
+                entity.Property(m => m.NormalizedUserName).HasMaxLength(127);
+                entity.Property(m => m.UserName).HasMaxLength(127);
+            });
+
+            builder.Entity<IdentityUserClaim<string>>().ToTable("user_claims");
+
+            builder.Entity<IdentityUserLogin<string>>().ToTable("user_logins");
+            builder.Entity<IdentityUserLogin<string>>(entity =>
+            {
+                entity.Property(m => m.LoginProvider).HasMaxLength(127);
+                entity.Property(m => m.ProviderKey).HasMaxLength(127);
+            });
+
+            builder.Entity<IdentityUserRole<string>>().ToTable("user_roles");
+            builder.Entity<IdentityUserRole<string>>(entity =>
+            {
+                entity.Property(m => m.UserId).HasMaxLength(127);
+                entity.Property(m => m.RoleId).HasMaxLength(127);
+            });
+
+            builder.Entity<IdentityUserToken<string>>().ToTable("user_tokens");
+            builder.Entity<IdentityUserToken<string>>(entity =>
+            {
+                entity.Property(m => m.UserId).HasMaxLength(127);
+                entity.Property(m => m.LoginProvider).HasMaxLength(127);
+                entity.Property(m => m.Name).HasMaxLength(127);
+            });
+
+            builder.Entity<IdentityRole>().ToTable("roles");
+            builder.Entity<IdentityRole>(entity => {
+                entity.Property(m => m.Id).HasMaxLength(127);
+                entity.Property(m => m.Name).HasMaxLength(127);
+                entity.Property(m => m.NormalizedName).HasMaxLength(127);
+            });
+
+            builder.Entity<IdentityRoleClaim<string>>().ToTable("role_claims");
+        }
+    }
+}
