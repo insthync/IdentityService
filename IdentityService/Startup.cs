f529403d86512c626a9d5658450a6a62e8b49b75
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2020-07-05 22:39:56 +0700

    Remove facebook and google identity extensions because they are not required anymore

--- IdentityService/Startup.cs
+++ IdentityService/Startup.cs
@@ -46,16 +46,6 @@ namespace IdentityService
                     options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
 
                 })
-                .AddFacebook(options =>
-                {
-                    options.AppId = Configuration["Authentication:Facebook:AppId"];
-                    options.AppSecret = Configuration["Authentication:Facebook:AppSecret"];
-                })
-                .AddGoogle(options =>
-                {
-                    options.ClientId = Configuration["Authentication:Google:ClientId"];
-                    options.ClientSecret = Configuration["Authentication:Google:ClientSecret"];
-                })
                 .AddJwtBearer(cfg =>
                 {
                     cfg.RequireHttpsMetadata = false;
